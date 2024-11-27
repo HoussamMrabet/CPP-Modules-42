6b6227374c4f509c70902ca7405f715a3f43da4a
HoussamMrabet <dodslim100@gmail.com>
2024-11-27 22:29:15 +0100

    fix

--- CPP-Module-05/ex03/Intern.cpp
+++ CPP-Module-05/ex03/Intern.cpp
@@ -6,11 +6,12 @@
 /*   By: hmrabet <hmrabet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 19:15:05 by hmrabet           #+#    #+#             */
-/*   Updated: 2024/11/27 20:32:01 by hmrabet          ###   ########.fr       */
+/*   Updated: 2024/11/27 22:27:24 by hmrabet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Intern.hpp"
+#include <map>
 
 const char		*Intern::InvalidFormException::what() const throw()
 {
@@ -32,15 +33,35 @@ Intern& Intern::operator=(Intern const &intern)
 
 Intern::~Intern() {}
 
+AForm *createRobotomyRequestForm(const std::string &target)
+{
+    return (new RobotomyRequestForm(target));
+}
+
+AForm *createShrubberyCreationForm(const std::string &target)
+{
+    return (new ShrubberyCreationForm(target));
+}
+
+AForm *createPresidentialPardonForm(const std::string &target)
+{
+    return (new PresidentialPardonForm(target));
+}
+
 AForm   *Intern::makeForm(std::string const &name, std::string const &target)
 {
-    if (!name.compare("robotomy request"))
-        return (new RobotomyRequestForm(target));
-    else if (!name.compare("shrubbery creation"))
-        return (new ShrubberyCreationForm(target));
-    else if (!name.compare("presidential pardon"))
-        return (new PresidentialPardonForm(target));
-    else
+    const std::map<std::string, AForm* (*)(const std::string&)> formCreators = 
+        {
+            {"robotomy request", createRobotomyRequestForm},
+            {"shrubbery creation", createShrubberyCreationForm},
+            {"presidential pardon", createPresidentialPardonForm}
+        };
+
+    std::map<std::string, AForm* (*)(const std::string&)>::const_iterator it = formCreators.find(name);
+    
+    if (it == formCreators.end())
         throw InvalidFormException();
+
+    return (it->second(target));
 }
 
