56f45ef67ac76d1fc3ec90c5ccfbccdf309b2ad1
HoussamMrabet <dodslim100@gmail.com>
2024-12-01 10:43:07 +0100

    add test

--- CPP-Module-07/ex02/main.cpp
+++ CPP-Module-07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hmrabet <hmrabet@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/01 01:20:53 by hmrabet           #+#    #+#             */
-/*   Updated: 2024/12/01 10:25:37 by hmrabet          ###   ########.fr       */
+/*   Updated: 2024/12/01 10:42:31 by hmrabet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,23 @@ int main()
         std::cout << std::endl;
 
         std::cout << intArray[3] << std::endl;
+    }
+	catch (const std::exception& e)
+	{
+        std::cerr << e.what() << std::endl;
+    }
+
+    try
+	{
+        Array<long> longArray(10);
+        for (unsigned int i = 0; i < longArray.getSize(); i++)
+            longArray[i] = i + 1;
+
+        for (unsigned int i = 0; i < longArray.getSize(); i++)
+            std::cout << longArray[i] << " ";
+        std::cout << std::endl;
+
+        std::cout << longArray[-1] << std::endl;
     }
 	catch (const std::exception& e)
 	{
